dfce8e4ebdced5305bf363d23a0f1f187ac35ab2
Burens Omondi <burensomondi@gmail.com>
2023-04-14 20:55:40 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using Info;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
