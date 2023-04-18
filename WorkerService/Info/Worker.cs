3431439a733afb228a164c802d0a770206cdda88
Burens Omondi <burensomondi@gmail.com>
2023-04-18 20:57:57 +0300

    added: JobMetadata model class

--- WorkerService/Info/Worker.cs
+++ WorkerService/Info/Worker.cs
@@ -1,3 +1,4 @@
+using Info.Models;
 using Info.Utils;
 using Quartz;
 using Quartz.Spi;
@@ -20,7 +21,7 @@ namespace Info
             this._jobMetadatas = jobMetadatas;
         }
 
-        public Task StartAsync(CancellationToken cancellationToken)
+        public async Task StartAsync(CancellationToken cancellationToken)
         {
             var logger = new Loggers();
 
@@ -29,6 +30,21 @@ namespace Info
                 //Creating Scheduler
                 Scheduler = await _schedulerFactory.GetScheduler(cancellationToken);
                 Scheduler.JobFactory = _jobFactory;
+
+                //Support for Multiple Jobs
+                _jobMetadatas.ForEach(jobMetadata =>
+                {
+                    /*//Create Job
+                    var jobDetail = CreateJob(jobMetadata);
+
+                    //Create trigger
+                    var trigger = CreateTrigger(jobMetadata);
+
+                    //Schedule Job
+                    Scheduler.ScheduleJob(jobDetail, trigger, cancellationToken).GetAwaiter();*/
+                });
+                //Start The Scheduler
+                await Scheduler.Start(cancellationToken);
             }
             catch (Exception ex)
             {
@@ -36,6 +52,8 @@ namespace Info
             }
             throw new NotImplementedException();
         }
+
+
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
