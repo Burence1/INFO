94d2c4d11bd143e60e15627131bdbc194f7fb90a
Burens Omondi <burensomondi@gmail.com>
2023-07-23 20:28:57 +0300

    update folder

---
+++ InfoApp/src/main/java/com/Info/InfoApp/auth/config/WebSecurityConfig.java
@@ -0,0 +1,14 @@
+package com.Info.InfoApp.auth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdabpter {
+}
